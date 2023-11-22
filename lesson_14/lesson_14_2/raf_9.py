c058b0536be9d5d011075c67bb2a93aa0c133e33
Oksana Ozturk <oksanaozturk@gmail.com>
2023-11-22 23:03:44 +0300

    Add raf_9.py

---
+++ lesson_14/lesson_14_2/raf_9.py
@@ -0,0 +1,31 @@
+class Raf9:
+
+    def __init__(self):
+        self.ingrediens = []
+
+    def __call__(self, *args, **kwargs):
+        with True:
+            self.__help_text()
+            command = input("Введите команду!")
+            if command == '0'
+                print("Всего доброго, приходите ещё!")
+
+            elif command == '1'
+                pass
+
+            else:
+                print("Не знаю такой команды")
+
+    def __help_text(self):
+        print("Доступны команды: ")
+        print("1 - выбрать ингредиенты")
+        print("0 - выход")
+
+    def choose_ingredients(self):
+        pass
+
+
+if __name__ == '__main__':
+    raf9 = Raf9()
+    raf9()
+
