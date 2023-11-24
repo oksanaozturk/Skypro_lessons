4203ab99b3f700339f5db84410c42d2ca1d9f887
Oksana Ozturk <oksanaozturk@gmail.com>
2023-11-24 22:22:09 +0300

    update super__.py and extension.py

--- lesson_15_1/extension.py
+++ lesson_15_1/extension.py
@@ -1,25 +1,20 @@
-from lesson_15_1.employee import Employee
+class Employee:
+
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+
+    def fullname(self):
+        return print(f'{self.first} {self.last}')
 
 
 class Developer(Employee):
-    raise_amt = 1.10
-
-    def __init__(self, first, last, pay, prog_lang):
-        super().__init__(first, last, pay)
-        self.prog_lang = prog_lang
-
-    def __add__(self, other):
-        if not isinstance(other, Employee):
-            raise ValueError('Складывать можно только объекты Employee и дочерние от них.')
-        return self.pay + other.pay
+    def code(self):
+        print("I'm coding now!")
 
 
 if __name__ == "__main__":
-    dev_1 = Developer('Ivan', 'Ivanov', 60000, 'Python')
-    dev_2 = Developer('Petr', 'Petrov', 70000, 'Java')
-    print(dev_1 + dev_2)
-
-    emp_1 = Employee('Petr', 'Petrov', 50000)
-    print(dev_1 + emp_1)
-
-    # print(dev_2 + 10000)
+    dev_1 = Developer('Ivan', 'Ivanov', 60000)
+    dev_1.fullname()  # Вызов метода базового класса Employee
+    dev_1.code()  # Вызов метода дочернего класса Developer
